8a4e61809d33a26738f06429baa7700eca5288c2
adored <perekrutq@gmail.com>
2022-08-24 00:12:43 +0500

    Только для файла A

---
+++ 27(trash)A.py
@@ -0,0 +1,18 @@
+f = open('27A.txt')
+n = int(f.readline())
+a = []
+for i in range(n):
+    x = int(f.readline())
+    a.append(x)
+
+sums = [0] * n
+mn = float('inf')
+
+# перебор всех возможных вариантов
+for i in range(n):
+    for j in range(n):
+        sums[i] += a[j] * min(abs(i - j), n - abs(i - j))
+    if sums[i] < mn:
+        mn = sums[i]
+        counter = i + 1
+print(counter)
